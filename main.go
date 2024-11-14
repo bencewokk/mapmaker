fa2dbc5d851845dbc893565f6aaeca96984551e9
Bencewok <bencewokk@gmail.com>
2024-11-14 22:24:30 +0100

    0.1

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
@@ -36,7 +37,7 @@ type gamemap struct {
 	// map data (2D array)
 	//
 	// 0 = not decided, 1 = mountains, 2 = plains, 3 = hills, 4 = forests
-	data [36][64]int
+	data [100][100]int
 }
 
 func gameinit() {
@@ -58,7 +59,7 @@ func (g *Game) Update() error {
 	return nil
 }
 
-func arrayToDeclaration(data [36][64]int) string {
+func arrayToDeclaration(data [1000][1000]int) string {
 	result := "data := [][]int{\n"
 	for _, row := range data {
 		result += "\t{"
@@ -79,24 +80,48 @@ var selected int
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		lcamera.pos.float_x -= 10
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		lcamera.pos.float_x += 10
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		lcamera.pos.float_y += 10
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		lcamera.pos.float_y -= 10
+	}
+
 	if ebiten.IsKeyPressed(ebiten.Key1) {
 		selected = 1
 	} else if ebiten.IsKeyPressed(ebiten.Key2) {
 		selected = 2
 	} else if ebiten.IsKeyPressed(ebiten.Key3) {
 		selected = 3
+	} else if ebiten.IsKeyPressed(ebiten.Key4) {
+		selected = 0
 	}
 
 	intmx, intmy := ebiten.CursorPosition()
-	cursor.float_x, cursor.float_y = float32(intmx), float32(intmy)
+	cursor.float_x, cursor.float_y = float32(intmx)-lcamera.pos.float_x+screenWidth/2, float32(intmy)-lcamera.pos.float_y+screenHeight/2
 	x, y := ptid(cursor)
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButton0) {
-		currentmap.data[y][x] = selected
+		if x >= 0 && y >= 0 {
+			currentmap.data[y][x] = selected
+		}
 	}
 
-	for i := 0; i < 36; i++ {
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButton1) {
 
-		for j := 0; j < 64; j++ {
+	}
+
+	for i := 0; i < 100; i++ {
+
+		for j := 0; j < 100; j++ {
 
 			switch currentmap.data[i][j] {
 			case 2:
@@ -114,8 +139,8 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 			vector.DrawFilledRect(
 				screen,
-				float32(j)*screendivisor,
-				float32(i)*screendivisor,
+				(float32(j)*screendivisor+lcamera.pos.float_x*lcamera.zoom)-screenWidth/2,
+				(float32(i)*screendivisor+lcamera.pos.float_y*lcamera.zoom)-screenHeight/2,
 				screendivisor*lcamera.zoom,
 				screendivisor*lcamera.zoom,
 				currenttilecolor,
@@ -124,10 +149,13 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		}
 	}
 
-	write()
+	fps := ebiten.CurrentFPS()
+	fpsText := fmt.Sprintf("FPS: %.2f", fps)
+	ebitenutil.DebugPrint(screen, fpsText)
 
-	fmt.Println()
-
+	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+		write()
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -136,8 +164,9 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func write() {
-	// Define the file name and content
+	// Define the file name
 	filename := "example.txt"
+
 	// Open the file (or create it if it doesn't exist) in write-only mode
 	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
@@ -146,15 +175,34 @@ func write() {
 	}
 	defer file.Close() // Ensure file is closed after writing is done
 
-	// Write content to the file
-	_, err = file.WriteString(arrayToDeclaration(currentmap.data))
-	if err != nil {
-		fmt.Println("Error writing to the file:", err)
-		return
+	// Iterate over each row of the map data (assuming `currentmap.data` is 100x100)
+	for _, row := range currentmap.data {
+		// Write each cell in the row separated by ", "
+		for j, value := range row {
+			if j > 0 {
+				_, err = fmt.Fprintf(file, ", ")
+				if err != nil {
+					fmt.Println("Error writing to the file:", err)
+					return
+				}
+			}
+			_, err = fmt.Fprintf(file, "%d", value)
+			if err != nil {
+				fmt.Println("Error writing to the file:", err)
+				return
+			}
+		}
+		// End the line after each row of numbers
+		_, err = fmt.Fprintf(file, "\n")
+		if err != nil {
+			fmt.Println("Error writing to the file:", err)
+			return
+		}
 	}
 
 	fmt.Println("File written successfully!")
 }
+
 func main() {
 	gameinit()
 	ebiten.SetWindowSize(1600, 900)
